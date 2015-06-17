ece41baa516550c4433aad540c2424d9399b6ffc
José Yukio Akazawa <joseyukio@users.noreply.github.com>
2015-06-17 18:24:45 -0300

    First commit. Test.

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,9 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+#Author: Jose Yukio Akazawa
+#Date:
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
